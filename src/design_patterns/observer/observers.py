ab2d8b2f3679bf4a897f46a97cf62d3189b6ecdd
Jasper Sival <jasper.sival@alliander.com>
2025-03-20 10:46:48 +0100

    Add StatisticsDisplay observer

--- src/design_patterns/observer/observers.py
+++ src/design_patterns/observer/observers.py
@@ -27,3 +27,26 @@ class CurrentConditionsDisplay(Observer):
         print(
             f"Current conditions: {self.temperature}\u00b0C  and {self.humidity}% humidity"
         )
+
+
+class StatisticsDisplay(Observer):
+    max_temperature: float
+    min_temperature: float
+    avg_temperature: float
+
+    def __init__(self, weather_data: "WeatherData"):
+        self.weather_data = weather_data
+        self.weather_data.register_observer(self)
+        self.temperatures: list[float] = []
+
+    def update(self):
+        self.temperatures.append(self.weather_data.temperature)
+        self.max_temperature = max(self.temperatures)
+        self.min_temperature = min(self.temperatures)
+        self.avg_temperature = sum(self.temperatures) / len(self.temperatures)
+        self.display()
+
+    def display(self):
+        print(
+            f"Avg/Max/Min temperature: {self.avg_temperature}/{self.max_temperature}/{self.min_temperature}\u00b0C"
+        )
