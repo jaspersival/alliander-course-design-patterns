66a4ce9be5284debae2237efa1c7df6e88020735
Jasper Sival <jasper.sival@alliander.com>
2025-03-20 09:56:15 +0100

    Add CurrentConditionsDisplay

--- src/design_patterns/observer/observers.py
+++ src/design_patterns/observer/observers.py
@@ -1,6 +1,29 @@
 from abc import ABC, abstractmethod
+import typing
+
+if typing.TYPE_CHECKING:
+    from .subjects import WeatherData
 
 
 class Observer(ABC):
     @abstractmethod
     def update(self): ...
+
+
+class CurrentConditionsDisplay(Observer):
+    temperature: float
+    humidity: float
+
+    def __init__(self, weather_data: "WeatherData"):
+        self.weather_data = weather_data
+        self.weather_data.register_observer(self)
+
+    def update(self):
+        self.temperature = self.weather_data.temperature
+        self.humidity = self.weather_data.humidity
+        self.display()
+
+    def display(self):
+        print(
+            f"Current conditions: {self.temperature}\u00b0C  and {self.humidity}% humidity"
+        )
