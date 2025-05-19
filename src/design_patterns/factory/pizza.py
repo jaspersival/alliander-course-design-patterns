3e9202aeb6451df57fdcb7b54f953665da1285f3
Jasper Sival <jasper.sival@alliander.com>
2025-05-19 15:26:39 +0200

    Add pizza as part of the factory pattern

---
+++ src/design_patterns/factory/pizza.py
@@ -0,0 +1,48 @@
+from abc import ABC
+
+
+class Pizza(ABC):
+    name: str
+    dough: str
+    sauce: str
+    toppings: list[str] = []
+
+    def prepare(self):
+        print(f"Preparing {self.name}")
+        print(f"Tossing dough: {self.dough}")
+        print(f"Adding sauce: {self.sauce}")
+        print("Adding toppings:")
+        for topping in self.toppings:
+            print(f"  {topping}")
+
+    @staticmethod
+    def bake():
+        print("Bake for 25 minutes at 350")
+
+    @staticmethod
+    def cut():
+        print("Cutting the pizza into diagonal slices")
+
+    @staticmethod
+    def box():
+        print("Place pizza in official PizzaStore box")
+
+
+class NYStyleCheesePizza(Pizza):
+    def __init__(self):
+        self.name = "NY Style Sauce and Cheese Pizza"
+        self.dough = "Thin Crust Dough"
+        self.sauce = "Marinara Sauce"
+        self.toppings.append("Grated Reggiano Cheese")
+
+
+class ChicagoStyleCheesePizza(Pizza):
+    def __init__(self):
+        self.name = "Chicago Style Deep Dish Cheese Pizza"
+        self.dough = "Extra Thick Crust Dough"
+        self.sauce = "Plum Tomato Sauce"
+        self.toppings.append("Shredded Mozzarella Cheese")
+
+    @staticmethod
+    def cut():
+        print("Cutting the pizza into square slices")
