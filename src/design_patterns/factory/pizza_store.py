322f70cada0114c4e0e4700d3dfb921b4b6b27bd
Jasper Sival <jasper.sival@alliander.com>
2025-05-20 10:00:45 +0200

    Implement pizza_store.py

---
+++ src/design_patterns/factory/pizza_store.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+from design_patterns.factory.pizza import (
+    NYStyleCheesePizza,
+    NYStyleVeggiePizza,
+    ChicagoStyleCheesePizza,
+    ChicagoStyleVeggiePizza,
+)
+
+
+class PizzaStore(ABC):
+    def order_pizza(self, pizza_type: str):
+        pizza = self.create_pizza(pizza_type)
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+        return pizza
+
+    @abstractmethod
+    def create_pizza(self, pizza_type: str):
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+
+class NYStylePizzaStore(PizzaStore):
+    def create_pizza(self, pizza_type: str):
+        if pizza_type == "cheese":
+            return NYStyleCheesePizza()
+        elif pizza_type == "veggie":
+            return NYStyleVeggiePizza()
+        else:
+            raise ValueError(f"Unknown pizza type: {pizza_type}")
+
+
+class ChicagoStylePizzaStore(PizzaStore):
+    def create_pizza(self, pizza_type: str):
+        if pizza_type == "cheese":
+            return ChicagoStyleCheesePizza()
+        elif pizza_type == "veggie":
+            return ChicagoStyleVeggiePizza()
+        else:
+            raise ValueError(f"Unknown pizza type: {pizza_type}")
