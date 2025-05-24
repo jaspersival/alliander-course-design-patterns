1369fe7aa7638d8283fc8de3acdc4d48d04604d8
Jasper Sival <jasper.sival@alliander.com>
2025-05-24 16:25:04 +0200

    Wire up factories in simple.py for franchises

--- src/design_patterns/factory/simple.py
+++ src/design_patterns/factory/simple.py
@@ -39,3 +39,19 @@ class PizzaStore:
         pizza.cut()
         pizza.box()
         return pizza
+
+
+class NYPizzaFactory(SimplePizzaFactory): ...
+
+
+class ChicagoPizzaFactory(SimplePizzaFactory): ...
+
+
+if __name__ == "__main__":
+    ny_factory = NYPizzaFactory()
+    ny_store = PizzaStore(ny_factory)
+    ny_store.order_pizza("cheese")
+
+    chicago_factory = ChicagoPizzaFactory()
+    chicago_store = PizzaStore(chicago_factory)
+    chicago_store.order_pizza("cheese")
